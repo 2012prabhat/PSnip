104f1a1667a5991d142427aebc94f3d9f2d32258
Prabhat Kumar <2012prabhat@gmail.com>
2023-09-09 22:07:39 +0530

    all file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Router from "./components/Router";
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router/>
+  );
 }
 
-export default App
+export function showErrorCard(errMessage,status){
+  if(document.querySelector('.errCard')) return;
+   let errCard = document.createElement('div');
+   errCard.setAttribute('class',`errCard ${status}`);
+   errCard.innerText = errMessage;
+   document.querySelector('body').appendChild(errCard);
+   setTimeout(()=>{
+    document.querySelector('.errCard').remove()
+   },2000)
+}
---
+++ src/components/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Login from './Login'
+import Dashboard from "./Dashboard"
+import "../css/main.css"
+import "../css/login.css"
+import "../css/dashboard.css"
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login/>}></Route>
+            <Route path="/dashboard" element={<Dashboard/>}></Route>
+        </Routes>
+    </BrowserRouter>
+  )
+}
